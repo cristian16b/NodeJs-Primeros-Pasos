4452a3635102fc4f06c91a8baf144924ca50145b
cristian16b <cristian.budzicz@gmail.com>
2020-04-13 12:03:53 -0300

    Ejercicio simple para comprobar cantidad de memoria, antes y despues de crear un vector

--- modulo os/ejercicio3.js
+++ modulo os/ejercicio3.js
@@ -9,3 +9,12 @@ console.log('Home directorio: ' + os.hostname());
 console.log('Info usuario: ' + JSON.stringify(os.userInfo()));
 console.log('Tipo sist. op.: ' + os.type());
 console.log('Cpus: ' + JSON.stringify(os.cpus()));
+
+// creo un vector y veo como cambia la memoria libre
+console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++');
+console.log('Memoria libre:'+os.freemem());
+const vec=[];
+for(let f=0;f<1000000;f++) {
+  vec.push(f);
+}	
+console.log('Memoria libre después de crear el vector:'+os.freemem());
