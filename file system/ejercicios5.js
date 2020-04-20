d3e175229feec9659b7c8a0bde7142c8f5062a75
cristian16b <cristian.budzicz@gmail.com>
2020-04-20 17:00:57 -0300

    Se agregan funciones para leer y crear archivos, se reviso asincronia y como se van ejecutando los hilos

--- file system/ejercicios5.js
+++ file system/ejercicios5.js
@@ -5,9 +5,12 @@ const nombreArchivo = 'miTxt';
 fs.stat(path, function(err) {
   if (!err) {
       console.log('file or directory exists');
+      // creo y escribo el nuevo archivo
+      crearArchivo();
+      leerArchivo();
   }
   else if (err.code === 'ENOENT') {
-      // console.log('file or directory does not exist');
+      console.log('file or directory does not exist');
       crearDirectorio();
   }
 });
@@ -18,5 +21,33 @@ crearDirectorio = () => {
     {
       console.log('Fallo creación del archivo en la ruta: ' + path);
     }
+    else {
+      console.log('Se creo el directorio');
+      // creo y escribo el nuevo archivo
+      crearArchivo();
+      leerArchivo();
+    }
   });
 }
+
+crearArchivo = () => {
+  let pathFile = path + '/' + nombreArchivo;
+  fs.writeFile(pathFile, 'línea 1\nLínea 2', error => {
+    if (error)
+      console.log(error);
+    else
+      console.log('El archivo fue creado');
+  });
+}
+
+leerArchivo = () => {
+  let pathFile = path + '/' + nombreArchivo;
+  fs.readFile(pathFile, (error,datos) => {
+    if (error)
+      console.log(error);
+    else
+      console.log(datos.toString());
+  });
+}
+
+console.log('fin del script');
